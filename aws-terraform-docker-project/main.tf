da1f74478fbf53f7ec9809f799d0a4ceef15c7d0
Rivce06 <gvenegas7978@gmail.com>
2025-08-19 23:03:09 +0000

    addingsecurity group

--- aws-terraform-docker-project/main.tf
+++ aws-terraform-docker-project/main.tf
@@ -45,6 +45,39 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
+
 resource "aws_instance" "web" {
   ami                         = "ami-0c2b8ca1dad447f8a"
   instance_type               = var.instance_type
