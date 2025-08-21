b957561a6d6d4cf5ceca4bdac97ea00cdb9fce8c
Rivce06 <gvenegas7978@gmail.com>
2025-08-21 22:37:39 +0000

    fix fmt backend

--- aws-terraform-docker-project/backend.tf
+++ aws-terraform-docker-project/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "andresr-devops-20250821"   
-    key            = "terraform/main.tfstate"    
+    bucket         = "andresr-devops-20250821"
+    key            = "terraform/main.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
