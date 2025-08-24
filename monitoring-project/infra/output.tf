efb13ccbe9bfd9438709b150ccbed073000f32d5
Rivce06 <gvenegas7978@gmail.com>
2025-08-24 00:58:38 +0000

    test

--- monitoring-project/infra/output.tf
+++ monitoring-project/infra/output.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
+  value       = aws_instance.nginx_server.public_ip
 }
 
 output "web_url" {
   description = "URL to access the web app"
-  value       = "http://${aws_instance.web.public_ip}"
+  value       = "http://${aws_instance.nginx_server.public_ip}"
 }
